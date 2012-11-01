7406829dbd2d9512132b6d67d5b43d4bcf84011c
Goran Mekić <goran.mekic@saturized.com>
2012-11-01 09:52:01 +0100

    Fix init script.

--- bin/init.sh
+++ bin/init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-VIM_ROOT_DIR="$(dirname $0)/.."
+VIM_ROOT_DIR="$(readlink -f $(dirname $0)/..)"
 
-cd
-rm -rf .vim .vimrc
-ln -s "${VIM_ROOT_DIR}/vim" .vim
-ln -s "${VIM_ROOT_DIR}/vimrc" .vimrc
+rm -rf ~/.vim ~/.vimrc
+ln -s "${VIM_ROOT_DIR}/vim" ~/.vim
+ln -s "${VIM_ROOT_DIR}/vimrc" ~/.vimrc
 cd "${VIM_ROOT_DIR}"
 git submodule init
 git submodule update
